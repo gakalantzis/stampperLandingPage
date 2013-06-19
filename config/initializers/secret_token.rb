30f240face30ebd53d283e4f97714e3e4dc993cc
George Kalantzis <georgekalantzis@android_f051cc5295653afd.gateway.2wire.net>
2013-06-18 19:13:38 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StampperLandingPage::Application.config.secret_token = 'da826a50567ac2529f6eb9b3b950ed093a4f00ef46a8be17ac1cf222534a1eb90fca522f1de0280740e24edf6e60630bb795fdc72c2e5149dccb7684acd759bd'
